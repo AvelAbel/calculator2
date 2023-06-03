c7209e1efad04f75669c3a6fb6cd11f870e62543
AvelAbel <Avel1996@gmail.com>
2023-06-03 21:11:37 +0300

    Кнопки готовы, все работает. Осталось задизайнить экран калькулятора, да и вообще все переделать

--- app/src/main/java/com/example/calculator2/MainActivity.kt
+++ app/src/main/java/com/example/calculator2/MainActivity.kt
@@ -24,60 +24,93 @@ class MainActivity : AppCompatActivity() {
         soundPool = SoundPool.Builder().setMaxStreams(2).build()  // Инициализируйте soundPool здесь
 
         // Здесь используйте soundPool
-        val sound1 = soundPool.load(this, R.raw.pressed, 1)
-        val sound2 = soundPool.load(this, R.raw.unpressed, 1)
+        val sound1 = soundPool.load(this, R.raw.pressandpush, 1)
+        val sound2 = soundPool.load(this, R.raw.pressandpush, 1)
 
 
-        // Обработчики событий для кнопок с числами
-        val numberButtons = listOf(
-            binding.button0, binding.button2, binding.button3,
-            binding.button4, binding.button5, binding.button6, binding.button7,
-            binding.button8, binding.button9
+        setButtonTouchListener(
+            binding.buttonDivide,
+            sound1,
+            sound2,
+            R.drawable.pressed_divide,
+            R.drawable.unpressed_divide,
+            {
+                currentExpression += "/"
+                binding.textView.text = currentExpression
+            }
         )
 
-        for ((index, button) in numberButtons.withIndex()) {
-            button.setOnClickListener {
-                currentExpression += index.toString()
+        setButtonTouchListener(
+            binding.buttonMultiply,
+            sound1,
+            sound2,
+            R.drawable.pressed_multiply,
+            R.drawable.unpressed_multiply,
+            {
+                // Добавляем операцию сложения в выражение
+                currentExpression += "*"
                 binding.textView.text = currentExpression
             }
-        }
-
-        // Обработчики событий для кнопок с операциями
-        val operationButtons = mapOf(
-            binding.buttonPlus to "+",
-            binding.buttonMinus to "-",
-            binding.buttonMultiply to "*",
-            binding.buttonDivide to "/"
         )
 
-        for ((button, operation) in operationButtons) {
-            button.setOnClickListener {
-                currentExpression += operation
+        setButtonTouchListener(
+            binding.buttonMinus,
+            sound1,
+            sound2,
+            R.drawable.pressed_minus,
+            R.drawable.unpressed_minus,
+            {
+                // Добавляем операцию сложения в выражение
+                currentExpression += "-"
                 binding.textView.text = currentExpression
             }
-        }
+        )
 
-        binding.buttonDot.setOnClickListener {
-            // Проверяем, содержит ли текущее число уже точку
-            if (!currentExpression.contains(".")) {
-                currentExpression += "."
+        setButtonTouchListener(
+            binding.buttonPlus,
+            sound1,
+            sound2,
+            R.drawable.pressed_plus,
+            R.drawable.unpressed_plus,
+            {
+                // Добавляем операцию сложения в выражение
+                currentExpression += "+"
                 binding.textView.text = currentExpression
             }
-        }
+        )
 
-        // Обработчик событий для кнопки равно
-        binding.buttonEquals.setOnClickListener {
-            val expression = Expression(currentExpression)
-            val result = expression.calculate()
-            if (result % 1 == 0.0) {
-                // Если результат является целым числом, преобразовать его в Int
-                currentExpression = result.toInt().toString()
-            } else {
-                currentExpression = result.toString()
+        setButtonTouchListener(
+            binding.buttonDot,
+            sound1,
+            sound2,
+            R.drawable.pressed_dot,
+            R.drawable.unpressed_dot,
+            {
+                if (!currentExpression.contains(".")) {
+                    currentExpression += "."
+                    binding.textView.text = currentExpression
+                }
             }
-            binding.textView.text = currentExpression
-        }
+        )
 
+        setButtonTouchListener(
+            binding.buttonEquals,
+            sound1,
+            sound2,
+            R.drawable.pressed_equall,
+            R.drawable.unpressed_equall,
+            {
+                val expression = Expression(currentExpression)
+                val result = expression.calculate()
+                if (result % 1 == 0.0) {
+                    // Если результат является целым числом, преобразовать его в Int
+                    currentExpression = result.toInt().toString()
+                } else {
+                    currentExpression = result.toString()
+                }
+                binding.textView.text = currentExpression
+            }
+        )
 
         setButtonTouchListener(
             binding.imageViewAC,
@@ -92,6 +125,37 @@ class MainActivity : AppCompatActivity() {
             }
         )
 
+
+        setButtonTouchListener(
+            binding.buttonPlusMinus,
+            sound1,
+            sound2,
+            R.drawable.pressed_minusplus,
+            R.drawable.unpressed_minusplus,
+            {
+                // Добавляем число 1 в выражение
+                val expression = Expression(currentExpression)
+                val result = -expression.calculate()
+                currentExpression = result.toString()
+                binding.textView.text = currentExpression
+            }
+        )
+
+        setButtonTouchListener(
+            binding.buttonPercent,
+            sound1,
+            sound2,
+            R.drawable.pressed_percent,
+            R.drawable.unpressed_percent,
+            {
+                // Добавляем число 1 в выражение
+                val expression = Expression(currentExpression)
+                val result = expression.calculate() / 100
+                currentExpression = result.toString()
+                binding.textView.text = currentExpression
+            }
+        )
+
         setButtonTouchListener(
             binding.button1,
             sound1,
@@ -99,28 +163,118 @@ class MainActivity : AppCompatActivity() {
             R.drawable.pressed1,
             R.drawable.unpressed1,
             {
-                // Добавляем число 1 в выражение
                 currentExpression += "1"
                 binding.textView.text = currentExpression
             }
         )
 
+        setButtonTouchListener(
+            binding.button2,
+            sound1,
+            sound2,
+            R.drawable.pressed2,
+            R.drawable.unpressed2,
+            {
+                currentExpression += "2"
+                binding.textView.text = currentExpression
+            }
+        )
 
-        // Обработчик событий для кнопки +/-
-        binding.buttonPlusMinus.setOnClickListener {
-            val expression = Expression(currentExpression)
-            val result = -expression.calculate()
-            currentExpression = result.toString()
-            binding.textView.text = currentExpression
-        }
+        setButtonTouchListener(
+            binding.button3,
+            sound1,
+            sound2,
+            R.drawable.pressed3,
+            R.drawable.unpressed3,
+            {
+                currentExpression += "3"
+                binding.textView.text = currentExpression
+            }
+        )
 
-        // Обработчик событий для кнопки %
-        binding.buttonPercent.setOnClickListener {
-            val expression = Expression(currentExpression)
-            val result = expression.calculate() / 100
-            currentExpression = result.toString()
-            binding.textView.text = currentExpression
-        }
+        setButtonTouchListener(
+            binding.button4,
+            sound1,
+            sound2,
+            R.drawable.pressed4,
+            R.drawable.unpressed4,
+            {
+                currentExpression += "4"
+                binding.textView.text = currentExpression
+            }
+        )
+
+        setButtonTouchListener(
+            binding.button5,
+            sound1,
+            sound2,
+            R.drawable.pressed5,
+            R.drawable.unpressed5,
+            {
+                currentExpression += "5"
+                binding.textView.text = currentExpression
+            }
+        )
+
+        setButtonTouchListener(
+            binding.button6,
+            sound1,
+            sound2,
+            R.drawable.pressed6,
+            R.drawable.unpressed6,
+            {
+                currentExpression += "6"
+                binding.textView.text = currentExpression
+            }
+        )
+
+        setButtonTouchListener(
+            binding.button7,
+            sound1,
+            sound2,
+            R.drawable.pressed7,
+            R.drawable.unpressed7,
+            {
+                currentExpression += "7"
+                binding.textView.text = currentExpression
+            }
+        )
+
+        setButtonTouchListener(
+            binding.button8,
+            sound1,
+            sound2,
+            R.drawable.pressed8,
+            R.drawable.unpressed8,
+            {
+                currentExpression += "8"
+                binding.textView.text = currentExpression
+            }
+        )
+
+        setButtonTouchListener(
+            binding.button9,
+            sound1,
+            sound2,
+            R.drawable.pressed9,
+            R.drawable.unpressed9,
+            {
+                currentExpression += "9"
+                binding.textView.text = currentExpression
+            }
+        )
+
+        setButtonTouchListener(
+            binding.button0,
+            sound1,
+            sound2,
+            R.drawable.pressed0,
+            R.drawable.unpressed0,
+            {
+                currentExpression += "0"
+                binding.textView.text = currentExpression
+            }
+        )
     }
 
     private fun setButtonTouchListener(
